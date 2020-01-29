aca61a8319afbdbc0d17f3992ca60f461ba827e2
Mehdi Khiati <mehdi1710@me.com>
2020-01-29 14:30:52 -0800

    working version

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,19 +1,40 @@
-from flask import Flask, render_template
-from .models import DB 
+from decouple import config
+from flask import Flask, render_template, request
+
+from .models import DB, User
+from .twitter import add_or_update_user
 
 
 def create_app():
-    """Create and configure an instance of the Flask application."""
+    """Create and configure an instance of the Flask application"""
     app = Flask(__name__)
-    app.config['SQLALCHMEY_DATABASE_URI'] = 'sqlite://///Users/Medy/Desktop/Twitoff/twitoff/db.sqlite3.'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
-    
+
     @app.route('/')
     def root():
-        return render_template('base.html', title = 'the space Jam!')
+        DB.create_all()
+        return render_template('base.html', title='Home', users=User.query.all())
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['username']
+        try:
+            if request.method == "POST":
+                add_or_update_user(name)
+                message = f'User {name} successfully added!'
+            tweets = User.query.filter(User.name==name).one().tweets
+        except Exception as e:
+            message = f'Error while try to add user {name}: {e}'
+            tweets = []
+        return render_template('user.html', title=name, message=message, tweets=tweets)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='Reset database!')
+
     return app
-
- 
-
-# if __name__ == "__main__":
-#     app.run(debug=True, port = 8080)
